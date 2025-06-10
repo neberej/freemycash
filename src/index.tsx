a5c0d9fec6313999b7d5b375e2c1d4d3566396f8
NT <git@nabaraj.com>
2025-06-10 13:38:59 -0700

    Fix landing page

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import "@fontsource/inter"; // Defaults to 400
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/700.css";
+
 import App from "./app/App";
 import "@src/styles/index.scss";
 
